4dffb259320de9d182f25c83a413a8c544c1ff04
Shane liew <shopping5501@gmail.com>
2022-08-10 14:42:11 -0500

    Aug_2

--- Aug_22/while.py
+++ Aug_22/while.py
@@ -1,6 +1,34 @@
-x = 0
+"""x = 0
 
 while x < 5:
     print(f'THe current value of x is {x}')
-    x = x + 1
+    x = x + 1   #x +=1
+else:
+    print("the value is 5")
+
+x = [1,2,3]
+
+for item in x:
+    #comment 
+    pass
+
+print ('end of script')
+
+
+
+mystring ="Sammy"
+
+for letter in mystring:
+    if letter =='a':
+        continue
+    print(letter)
+
+"""
+mystring ="Sammy"
+
+for letter in mystring:
+    if letter =='a':
+        break
+    print(letter)
+
 
