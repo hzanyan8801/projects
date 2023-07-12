26048ee12b974bf0e897c470369e75415a105631
shopp <shopping5501@gmail.com>
2023-07-12 08:10:20 -0500

    Day 23: send emails using the form

--- app2-portfolio/pages/Contact_US.py
+++ app2-portfolio/pages/Contact_US.py
@@ -1,10 +1,20 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email add")
-    message = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
+    #message = message + "\n" + user_email
+    message = f"""\
+Subject: New email from {user_email}
+
+from: {user_email}
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
+    print(button)
     if button:
-        print("I was pressed!")
+        send_email(message)
+        st.info("Your email was sent successfully")
--- app2-portfolio/send_email.py
+++ app2-portfolio/send_email.py
@@ -1,20 +1,17 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "shopping5501@gmail.com"
-password = "eofsdodycoufnhck"
+def send_email(message):
+    #global host, port, username, password, context, message, server
+    host = "smtp.gmail.com"
+    port = 465
+    username = "shopping5501@gmail.com"
+    password = "eofsdodycoufnhck"
+    receiver = "shopping5501@gmail.com"
+    context = ssl.create_default_context()
 
-receiver = "shopping5501@gmail.com"
-context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-message = """\
-Subject: Hi
-How are you?
-Bye!
-"""
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
