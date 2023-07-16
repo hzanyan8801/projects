e076a1ad81af7a261b68c6bc6f4e85ca5cc38b5c
shopp <shopping5501@gmail.com>
2023-07-16 17:40:05 -0500

    Day 27: send an email with news titles and descriptions

---
+++ app-5-api/send_email.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+
+
+def send_email(message):
+    #global host, port, username, password, context, message, server
+    host = "smtp.gmail.com"
+    port = 465
+    username = "shopping5501@gmail.com"
+    password = "eofsdodycoufnhck"
+    receiver = "shopping5501@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
