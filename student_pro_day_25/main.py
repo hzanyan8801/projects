32064927b663e75bde0c0247da2b5c0b29a22aa7
shopp <shopping5501@gmail.com>
2023-07-16 08:21:36 -0500

    Day 26: Add an invoice date

---
+++ student_pro_day_25/main.py
@@ -0,0 +1,25 @@
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+# Create a list of text filepaths
+filepaths = glob.glob("Text+Files/*.txt")
+# Create one PDF file
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+# Gp through each text file
+for filepath in filepaths:
+    # Add a page to the PDF document for each text file
+    pdf.add_page()
+
+    # Get the filename without the extension
+    # add convert it to title case (e.g. Cat)
+    filename = Path(filepath).stem
+    name = filename.title()
+
+    # Add the name to the PDF
+    pdf.set_font(family="times", size=16, style="B")
+    pdf.cell(w=58, h=8, txt=name, ln=1)
+
+# Produce the PDF
+pdf.output("output.pdf")
