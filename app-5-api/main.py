5a2c776a0e9b201999e58753bf32cf7d4d894522
shopp <shopping5501@gmail.com>
2023-07-16 22:56:24 -0500

    Day 29: build a website using Flask

--- app-5-api/main.py
+++ app-5-api/main.py
@@ -4,11 +4,11 @@ from send_email import send_email
 topic = "tesla"
 api_key = "d7eb34408b234a298813627b61643806"
 
-url = "https://newsapi.org/v2/everything?"\
-    f"q={topic}&"\
-    "from=2023-06-16&sortBy=publishedAt&"\
-    "apiKey=d7eb34408b234a298813627b61643806&"\
-    "language=en"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2023-06-16&sortBy=publishedAt&" \
+      "apiKey=d7eb34408b234a298813627b61643806&" \
+      "language=en"
 
 # Make request
 request = requests.get(url)
@@ -19,13 +19,13 @@ content = request.json()
 # Access the article titles and description
 body = ""
 for article in content["articles"][:20]:
-    #print(article["title"])
-    #print(article["description"])
+    # print(article["title"])
+    # print(article["description"])
     if article["title"] is not None:
         body = "Subject: Today's news" \
-               + "\n"+ body + article["title"] + "\n"  \
+               + "\n" + body + article["title"] + "\n" \
                + article["description"] \
-               + "\n" + article["url"] + 2*"\n"
+               + "\n" + article["url"] + 2 * "\n"
 
 body = body.encode("utf-8")
 send_email(body)
