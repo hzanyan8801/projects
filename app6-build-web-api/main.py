315b0acfb39c656fa73bfbc5e279c40a36cbeed5
shopp <shopping5501@gmail.com>
2023-07-17 10:56:05 -0500

    day 33: student api dictionary

--- app6-build-web-api/main.py
+++ app6-build-web-api/main.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template
 
 app = Flask("Website")
 
+
 @app.route("/home")
 def home():
     return render_template("tutorial.html")
+
 
 @app.route("/about/")
 def about():
     return render_template("about.html")
 
-app.run(debug=True)
+
+app.run(debug=True, port=5001)
